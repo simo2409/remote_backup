9555f5c0bc6ba125bb47e099cc59170b63b0b827
Simone Dall'Angelo <simone@wonsys.net>
2011-06-13 11:26:26 +0200

    Added feature to delete old backup file from destination

--- backup.rb
+++ backup.rb
@@ -6,6 +6,9 @@ raise 'Unable to find config.yml' unless File.exist?('./config.yml')
 # Loading config file
 config = YAML::load(File.open('./config.yml'))
 
+# Loading ActiveSupport if needed
+require 'active_support/all' if config['clear_old_backups']
+
 # Checking config values
 raise "Unable to find tar (it should be #{config['tar_bin']})" unless File.exist?(config['tar_bin'])
 raise "Unable to find directory to backup (it should be #{config['dir_to_backup']})" unless File.exist?(config['dir_to_backup'])
@@ -14,8 +17,10 @@ raise "Unable to find temporary path (it should be #{config['temporary_path']})"
 config['fixed_name'] = '' unless config['fixed_name']
 config['silent'] = false  unless config['silent']
 
+now = Time.now
+
 # Creates dynamic backup name based on date/hour. 201105121627.tar.gz means 2011-05-12 16:27
-config['fixed_name'] = Time.now.strftime("%Y%m%d%H%M") + '.tar.gz' if config['fixed_name'].empty?
+config['fixed_name'] = now.strftime("%Y%m%d%H%M") + '.tar.gz' if config['fixed_name'].empty?
 
 # Checks data for remote backup
 if config['remote_backup']
@@ -45,6 +50,12 @@ if config['remote_backup']
     ftp = Net::FTP.new(config['remote_host'], config['remote_username'], config['remote_password'])
     ftp.chdir(config['destination_path'])
     ftp.put(full_temporary_path)
+    if config['clear_old_backups']
+      # Cleaning old backups
+      old_backup_to_delete = (now - 3.days).strftime("%Y%m%d%H%M") + '.tar.gz'
+      ftp.delete(old_backup_to_delete)
+    end
+    ftp.close
   rescue Net::FTPPermError
     puts 'ERROR (Permission problem)'
     exit
@@ -55,7 +66,17 @@ else
   if system('cp ' + full_temporary_path + ' ' + full_destination_path)
     puts 'OK' unless config['silent']
   else
-    puts 'ERROR' unless config['silent']
+    puts 'ERROR'
+  end
+  if config['clear_old_backups']
+    # Cleaning old backups
+    old_backup_to_delete = (now - 7.days).strftime("%Y%m%d") + '*'
+    print "Deleting old backup .. " unless config['silent']
+    if system('rm ' + File.join(config['destination_path'], old_backup_to_delete))
+      puts 'OK' unless config['silent']
+    else
+      puts 'ERROR'
+    end
   end
 end
 
@@ -64,5 +85,5 @@ print 'Cleaning temp file .. ' unless config['silent']
 if system('rm ' + full_temporary_path)
   puts 'OK' unless config['silent']
 else
-  puts 'ERROR' unless config['silent']
+  puts 'ERROR'
 end
