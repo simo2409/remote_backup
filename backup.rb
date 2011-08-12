ecf0b82a148be55018e474ea9ac441ebd2c1c15b
Simone Dall'Angelo <simobg@gmail.com>
2011-08-12 10:14:25 +0200

    Added remote_port to config + Removed clear_old_backup, I didn't apreciate it

--- backup.rb
+++ backup.rb
@@ -11,27 +11,6 @@ config = YAML::load(File.open(config_file))
 now = Time.now
 date_format = "%Y%m%d%H%M"
 
-if config['clear_old_backups']
-  # Loading ActiveSupport if needed
-  require 'active_support/all'
-  
-  def generate_list_of_deleting_files(now, date_format, config, all_files, format = 'local')
-    if config['preserve_list_of_old_backup'].any?
-      config['preserve_list_of_old_backup'] << 0 # 0 identifies the today's backup
-      to_preserve = []
-      config['preserve_list_of_old_backup'].each do |save_me|
-        if format == 'local'
-          to_preserve << File.join(config['destination_path'], 'backup' + (now - save_me.days).strftime(date_format) + '.tar.gz')
-        else
-          to_preserve << 'backup' + (now - save_me.days).strftime(date_format) + '.tar.gz'
-        end
-      end
-    end
-    return (all_files - to_preserve)
-  end
-  
-end
-
 # Checking config values
 raise "Unable to find tar (it should be #{config['tar_bin']})" unless File.exist?(config['tar_bin'])
 raise "Unable to find directory to backup (it should be #{config['dir_to_backup']})" unless File.exist?(config['dir_to_backup'])
@@ -39,14 +18,19 @@ raise "Unable to find destination path (it should be #{config['destination_path'
 raise "Unable to find temporary path (it should be #{config['temporary_path']})" unless File.exist?(config['temporary_path'])
 config['fixed_name'] = '' unless config['fixed_name']
 config['silent'] = false  unless config['silent']
-raise "There is a non expected value in 'preserve_list_of_old_backup'" if config['clear_old_backups'] && (!config['preserve_list_of_old_backup'].is_a?(Array) || config['preserve_list_of_old_backup'].select {|i| !i.is_a?(Fixnum)}.any?)
 
 # Checks data for remote backup
 if config['remote_backup']
-  raise 'FTP host not set'      unless config['remote_host'] || config['remote_host'].empty?
-  raise 'FTP username not set'  unless config['remote_username'] || config['remote_username'].empty?
-  raise 'FTP password not set'  unless config['remote_password'] || config['remote_password'].empty?
-  require 'net/ftp'
+  config['remote_type'] = config['remote_type'].downcase
+  raise 'remote_type not set' unless config['remote_type']
+  raise 'unknown remote_type value' unless ['ftp', 'sftp'].include?(config['remote_type'])
+  
+  raise "#{config['remote_type'].upcase} host not set"      unless config['remote_host'] || config['remote_host'].empty?
+  raise "#{config['remote_type'].upcase} username not set"  unless config['remote_username'] || config['remote_username'].empty?
+  raise "#{config['remote_type'].upcase} password not set"  unless config['remote_password'] || config['remote_password'].empty?
+  raise "#{config['remote_type'].upcase} port not set"      unless config['remote_port'] || config['remote_port'].empty?
+  require 'net/ssh' if config['remote_type'] == 'sftp'
+  require "net/#{config['remote_type']}"
 end
 
 # Creates dynamic backup name based on date/hour. backup201105121627.tar.gz means 2011-05-12 16:27
@@ -80,28 +64,15 @@ print "Moving #{config['fixed_name']} to " unless config['silent']
 if config['remote_backup']
   begin
     print "FTP (#{config['remote_host']}) .. " unless config['silent']
-    ftp = Net::FTP.new(config['remote_host'], config['remote_username'], config['remote_password'])
-    ftp.chdir(config['destination_path'])
-    ftp.put(full_temporary_path)
-    puts "OK" unless config['silent']
-    if config['clear_old_backups']
-      # Cleaning old backups
-      all_remote_files = ftp.list
-      all_files = []
-      all_remote_files.each do |file|
-        got = file.match(/.+ (backup[0-9]{8}.tar.gz)/)
-        all_files << got[1] if got && got[1]
-      end
-      old_backups_to_delete = generate_list_of_deleting_files(now, date_format, config, all_files, 'ftp')
-      if old_backups_to_delete.any?
-        puts "Deleting old backup .. " unless config['silent']
-        old_backups_to_delete.each do |file_to_delete|
-          print "Deleting '#{file_to_delete}' .. "
-          ftp.delete(file_to_delete)
-          puts 'OK' unless config['silent']
-        end
-      end
+    if config['remote_type'] == 'ftp'
+      ftp = Net::FTP.new(config['remote_host'], config['remote_username'], config['remote_password'])
+      ftp.chdir(config['destination_path'])
+      ftp.put(full_temporary_path)
+    else
+      sftp = Net::SFTP.start(config['remote_host'], config['remote_username'], :password => config['remote_password'], :port => config['remote_port'])
+      sftp.put_file full_temporary_path full_destination_path
     end
+    puts "OK" unless config['silent']
     ftp.close
   rescue Net::FTPPermError
     puts 'ERROR (Permission problem)'
@@ -116,22 +87,6 @@ else
       puts 'OK' unless config['silent']
     else
       puts 'ERROR'
-    end
-  end
-  if config['clear_old_backups']
-    # Cleaning old backups
-    all_files = Dir.glob(File.join(config['destination_path'], 'backup*.tar.gz'))
-    old_backups_to_delete = generate_list_of_deleting_files(now, date_format, config, all_files)
-    if old_backups_to_delete.any?
-      puts "Deleting old backup .. " unless config['silent']
-      old_backups_to_delete.each do |file_to_delete|
-        print "Deleting '#{file_to_delete}' .. "
-        if system('rm ' + file_to_delete)
-          puts 'OK' unless config['silent']
-        else
-          puts 'ERROR'
-        end
-      end
     end
   end
 end
